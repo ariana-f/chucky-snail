131f661e8914c9be4871c034d0e6a3e6b6ad5e89
ariana-f <80515824+ariana-f@users.noreply.github.com>
2021-05-27 15:34:33 +0100

    anims

--- js/level001.js
+++ js/level001.js
@@ -1,13 +1,26 @@
+import { Player } from "./player.js";
+
 export class Level001 extends Phaser.Scene {
     constructor() {
         super('Level001');
     }
 
     init() {
-
+        this.controls = this.input.keyboard.createCursorKeys();
     }
 
     create() {
         this.add.image(0, 0, 'background').setOrigin(0).setScale(2);
+        
+        this.player = new Player(
+            this,
+            this.game.config.width * 0.5,
+            this.game.config.height * 0.5,
+            'player', 6
+        );
+    }
+
+    update(time) {
+        this.player.update(time);
     }
 }
--- js/loadscene.js
+++ js/loadscene.js
@@ -5,9 +5,39 @@ export class LoadScene extends Phaser.Scene {
 
     preload() {
         this.load.image('background', './images/blue_land.png');
+        this.load.spritesheet('player', './images/player.png', {
+            frameWidth: 128,
+            frameHeight: 144
+        })
+
     }
 
     create() {
-        this.scene.start
+        this.createAnimations();
+
+        this.scene.start('Level001');
+    }
+
+    createAnimations() {
+        this.anims.create({
+            key: 'walking',
+            frames: this.anims.generateFrameNames('player', {
+                frame: [9, 10]
+            }),
+            frameRate: 4,
+            yoyo: true,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key:'climbing',
+            frames: this.anims.generateFrameNames('player',{
+                frames:[0, 1]
+            }),
+            frameRate: 4,
+            yoyo: true,
+            repeat: -1
+
+        });
     }
 }
